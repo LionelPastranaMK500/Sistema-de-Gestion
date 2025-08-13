7a2faf1f7e861e14e01e684bed744a84844e1ebd
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2025-08-13 17:36:05 -0500

    fix(App): conflicto de merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./modules/auth/Login";
-<<<<<<< HEAD
 import Register from "./modules/auth/Register";
-=======
 import './index.css'; 
->>>>>>> 9916f679541022c8639efd2fdde2da62f712d65d
 
 export default function App(){
   return (
-<<<<<<< HEAD
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
-=======
-      <Login />
->>>>>>> 9916f679541022c8639efd2fdde2da62f712d65d
   );
 }
 
