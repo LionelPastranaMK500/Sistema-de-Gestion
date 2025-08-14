7929ed5fea0c51feea90aaac9e7f291e27b1f436
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2025-08-14 11:58:18 -0500

    refactor(routing): mover definición de rutas a AppRoutes se simplifico App y main

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-import Login from "./modules/auth/Login";
-import Register from "./modules/auth/Register";
+import AppRoutes from './routes/AppRoutes';
 import './index.css'; 
 
 export default function App(){
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <AppRoutes/>
   );
 }
-
