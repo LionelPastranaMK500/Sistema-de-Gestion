fdd304024ed340003a928d13a3ad6da1165bf453
LionelPastranaMK500 <andriypastrana1@gmail.com>
2025-08-14 14:08:59 -0500

    feat(colors): agregar constantes de colores para botones

---
+++ src/constants/colorsConstants.js
@@ -0,0 +1,11 @@
+export const buttonColors = [
+    "bg-green-500",   // Estadísticas
+    "bg-teal-600",    // Realizar venta
+    "bg-yellow-500",  // Ventas realizadas
+    "bg-purple-500",  // Proformas
+    "bg-red-500",     // Reportes
+    "bg-pink-500",    // Clientes y proveedores
+    "bg-cyan-500",    // Productos y servicios
+    "bg-gray-500",    // Configuración
+    "bg-orange-500"   // Soporte / Ayuda
+];
