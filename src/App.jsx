b1167e51deffa507751ab3abd5d536b36ece69e4
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2025-08-16 22:43:48 -0500

    feat(core): agregar ToastContainer global

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import AppRoutes from './routes/AppRoutes';
+import AppRoutes from "./routes/AppRoutes";
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import './index.css'; 
 
 export default function App(){
   return (
+    <>
     <AppRoutes/>
+    <ToastContainer position="top-right"/>
+    </>
   );
 }
