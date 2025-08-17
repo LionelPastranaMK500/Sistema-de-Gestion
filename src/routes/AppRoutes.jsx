2c503a62e101c46958b95e5e945ac5a870924a63
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2025-08-16 22:41:53 -0500

    feat(routes): agregar ruta para Ventas y ajustar MainLayout

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import MainLayout from "../layouts/MainLayout";
 import Dashboard from "../modules/dashboard/Dashboard";
 import Login from "../modules/auth/Login";
 import Register from "../modules/auth/Register";
+import VentasView from "../modules/ventas/VentasView";
 
 export default function AppRoutes() {
   return (
@@ -10,8 +11,9 @@ export default function AppRoutes() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
-        <Route path="/dashboard" element={<MainLayout />}>
-          <Route index element={<Dashboard />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/ventas" element={<VentasView/>}/>
         </Route>
       </Routes>
   );
