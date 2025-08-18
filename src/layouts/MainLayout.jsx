f142b7b14623845690f9a13d517b7da391a999ff
LionelPastranaMK500 <andriypastrana1@gmail.com>
2025-08-17 19:31:57 -0500

    refactor(MainLayout): mejorar estructura y estilos del layout principal

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 
-export default function MainLayout(){
-    return(
-        <div>
-            <Sidebar/>
-            <main>
-                <Outlet/>
-            </main>
+
+export default function MainLayout() {
+    return (
+        <div className="flex">
+            {/* Sidebar fijo */}
+            <Sidebar />
+
+            {/* Contenido dinámico */}
+            <div className="flex-1 p-6">
+                <Outlet />
+            </div>
         </div>
     );
 }
