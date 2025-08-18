2461c945d7a1cf6c1c66e2a250f85915c340f4d4
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2025-08-18 14:01:17 -0500

    chore(config): extender globals en eslint y agregar alias en vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(process.cwd(), "src"),
+      "@constants": path.resolve(process.cwd(), "src/constants"),
+      "@components": path.resolve(process.cwd(), "src/components"),
+      "@modules": path.resolve(process.cwd(), "src/modules"),
+      "@routes": path.resolve(process.cwd(), "src/routes"),
+      "@services": path.resolve(process.cwd(), "src/services"),
+      "@utils": path.resolve(process.cwd(), "src/utils"),
+      "@hooks": path.resolve(process.cwd(), "src/hooks"),
+      "@layouts": path.resolve(process.cwd(), "src/layouts"),
+    },
+  },
 })
