76e0a9bc467a60b513a5c5011feac6a1e794cc80
LionelPastranaMK500 <andriypastrana1@gmail.com>
2025-08-21 11:22:09 -0500

    cambios en main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "@components/Sidebar/Sidebar";
-
+import SidebarSkeleton from "@components/Sidebar/SidebarSkeleton";
+import { useSidebar } from "@utils/sidebarState";
 
 export default function MainLayout() {
+    const { sidebarReady } = useSidebar();
+
     return (
         <div className="flex">
-            {/* Sidebar fijo */}
-            <Sidebar />
+            {/* Sidebar o Skeleton */}
+            {sidebarReady ? <Sidebar /> : <SidebarSkeleton />}
 
             {/* Contenido dinámico */}
             <main className="flex-1 bg-gray-50 p-6 min-w-0 overflow-auto">
