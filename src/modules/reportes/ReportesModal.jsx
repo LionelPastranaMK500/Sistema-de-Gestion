63ff44bb5dc30650c36f73b21faf94dcc8c01430
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2025-08-28 00:21:43 -0500

    feat(reportes): implementar vista inicial y estructura de reportes

---
+++ src/modules/reportes/ReportesModal.jsx
@@ -0,0 +1,7 @@
+export default function ReportesModal() {
+    return (
+        <div>
+
+        </div>
+    )
+}
--- src/modules/reportes/ReportesView.jsx
+++ src/modules/reportes/ReportesView.jsx
@@ -1,7 +1,25 @@
+import { menuItemsReportes } from "@constants/menuItemsConstants";
+import { menuActionsReportes } from "@utils/menuActions";
+import { useNavigate } from "react-router-dom";
+
 export default function ReportesView() {
+    const navigate = useNavigate();
+
     return (
         <div>
-
+            <h2>Reportes</h2>
+            <div>
+                {menuItemsReportes.map((item, index) => {
+                    const Icon = item.icon;
+                    return (
+                        <button key={index} onClick={() => menuActionsReportes[item.action]({ navigate })}>
+                            <Icon></Icon>
+                            <span>{item.name}</span>
+                            <span>{item.description}</span>
+                        </button>
+                    )
+                })}
+            </div>
         </div>
     );
 }
