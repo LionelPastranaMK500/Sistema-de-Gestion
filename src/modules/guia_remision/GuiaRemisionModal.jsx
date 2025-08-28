b2fe0f76c5c19431593c101a422d85764dd5d659
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2025-08-28 00:20:25 -0500

    feat(guia-remision): agregar vistas y modal para gestión de guías de remisión

---
+++ src/modules/guia_remision/GuiaRemisionModal.jsx
@@ -0,0 +1,106 @@
+// import { useState } from "react";
+import { CloseIcon } from "@constants/iconsConstants";
+
+export default function GuiaRemisionModal() {
+    return (
+        <div>
+            {/* Overlay oscuro */}
+            <div
+            ></div>
+
+            {/* Contenido del modal */}
+            <div >
+                {/* Header */}
+                <div >
+                    <h3 >
+                        Comprobante electrónico
+                    </h3>
+                    <CloseIcon
+                    />
+                </div>
+
+                {/* Body con scroll */}
+                <div >
+                    {/* Documento */}
+                    <div>
+                        <p>Guia de Remision Remitente Electronica</p>
+                        <p></p>
+                    </div>
+
+                    {/* Destinatario */}
+                    <div>
+                        <p>Destinatario</p>
+                        <p></p>
+                        <p></p>
+                    </div>
+
+                    {/* Origen / Destino */}
+                    <div>
+                        <p>Origen / Destino</p>
+                        <p></p>
+                        <p></p>
+                    </div>
+
+                    <div>
+                        <p>Usuario</p>
+                        <p></p>
+                        <p></p>
+                    </div>
+                    {/* Estado SUNAT */}
+                    <div>
+                        <strong>ESTADO SUNAT</strong>
+                        <p>Aceptado</p>
+                    </div>
+
+                    {/* Botonera */}
+                    <div>
+                        <button>
+                            ENVIAR POR EMAIL
+                        </button>
+                        <button>
+                            ENVIAR POR WHATSAPP
+                        </button>
+                        <button>
+                            VISUALIZAR PDF
+                        </button>
+
+                        <div>
+                            <button
+                                //onClick={() => setMostrarFormatos(!mostrarFormatos)}
+                            >
+                                DESCARGAR PDF
+                            </button>
+
+                            {/* {mostrarFormatos && (
+                                <ul className="right-0 left-0 z-50 absolute bg-white shadow-lg mt-1 border border-gray-200 rounded">
+                                    {formatos.map((formato) => (
+                                        <li key={formato.id}>
+                                            <button
+                                                className="hover:bg-gray-100 px-4 py-2 w-full text-left"
+                                                onClick={() => handleSeleccionFormato(formato.id)}
+                                            >
+                                                {formato.label}
+                                            </button>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )} */}
+                        </div>
+                       
+                        <button>
+                            DESCARGAR XML
+                        </button>
+                        <button >
+                            DESCARGAR CDR
+                        </button>
+                    </div>
+
+                    {/* Botón de baja (rojo) */}
+                    <button>
+                       ANULAR
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/modules/guia_remision/GuiaRemisionView.jsx
@@ -0,0 +1,139 @@
+import {
+    MoreVertIcon,
+    PermIdentityTwoToneIcon,
+    AccessTimeTwoToneIcon
+} from "@constants/iconsConstants";
+import { useState, useEffect, useRef } from "react";
+import { configCalendar } from "@utils/configCalendar";
+import { Calendar } from "primereact/calendar";
+import GuiaRemisionModal from "./GuiaRemisionModal";
+
+
+export default function GuiaRemision() {
+    const [guia_remision] = useState([]);
+    const [selectedGuiaRemision, setSelectedGuiaRemision] = useState(null);
+    const [fechaSeleccionada, setFechaSeleccionada] = useState(null);
+
+    const calendarRef = useRef(null);
+
+    const getTituloFecha = (fecha) => {
+        if (!fecha) return "Proformas";
+        const diaSemana = fecha.toLocaleDateString("es-ES", { weekday: "long" });
+        const dia = fecha.getDate();
+        const mes = fecha.toLocaleDateString("es-ES", { month: "long" });
+        return `${diaSemana.charAt(0).toUpperCase() + diaSemana.slice(1)} » ${dia}° ${mes.charAt(0).toUpperCase() + mes.slice(1)}`;
+    };
+
+    const guiaRemisionFiltradas = fechaSeleccionada
+        ? guia_remision.filter((f) => {
+            const fechaGuiaRemision = new Date(f.fecha);
+            return (
+                fechaGuiaRemision.getDate() === fechaSeleccionada.getDate() &&
+                fechaGuiaRemision.getMonth() === fechaSeleccionada.getMonth() &&
+                fechaGuiaRemision.getFullYear() === fechaSeleccionada.getFullYear()
+            );
+        })
+        : [];
+
+    const formatoFechaHora = (fechaISO) => {
+        if (!fechaISO) return "";
+        const fecha = new Date(fechaISO);
+        const opcionesFecha = { day: "numeric", month: "long" };
+        const fechaFormateada = fecha.toLocaleDateString("es-ES", opcionesFecha).replace(/\b\w/, c => c.toUpperCase());
+        const horaFormateada = fecha.toLocaleTimeString("es-ES", {
+            hour: "numeric",
+            minute: "2-digit",
+            hour12: true
+        });
+        return `${fechaFormateada}, ${horaFormateada}`;
+    };
+
+    useEffect(() => {
+        configCalendar();
+    }, []);
+
+    return (
+        <div>
+            <div>
+                <h2>
+                    {getTituloFecha(fechaSeleccionada)}
+                </h2>
+
+                <div>
+                    <Calendar
+                        ref={calendarRef}
+                        value={fechaSeleccionada}
+                        onChange={(e) => setFechaSeleccionada(e.value)}
+                        dateFormat="dd/mm/yy"
+                        showIcon
+                    />
+
+                    <button onClick={() => calendarRef.current.show()}>
+                        <i></i>
+                    </button>
+
+                    <button>
+                        Registrar Nuevo
+                    </button>
+
+                </div>
+            </div>
+
+            <div>
+                {fechaSeleccionada ? (
+                    guiaRemisionFiltradas.length > 0 ? (
+                        <div>
+                            {guiaRemisionFiltradas.map((f) => (
+                                <div
+                                    key={f.id}
+                                    onClick={() => setSelectedGuiaRemision(f)}
+                                >
+                                    <div>
+                                        <p>
+                                            {f.tDocumento} » {f.numero}
+                                        </p>
+                                        <p>
+                                            <PermIdentityTwoToneIcon fontSize="small" />
+                                            <span>
+                                                {f.cliente} ({f.ruc})
+                                            </span>
+                                        </p>
+                                        <p>
+                                            <AccessTimeTwoToneIcon fontSize="small" />
+                                            {formatoFechaHora(f.fecha)}
+                                        </p>
+                                    </div>
+
+                                    <div>
+                                        <p>
+                                            S/ {Math.abs(f.monto).toFixed(2)}
+                                        </p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <div>
+                            <img
+                                src="/assets/personaje.png"
+                                alt="Sin Guia de Remision"
+                            />
+                            <p>No se encontraron Guia de Remision realizadas en esta fecha</p>
+                        </div>
+                    )
+                ) : (
+                    <div>
+                        <p>Seleccione una fecha para ver las Guias de Remision.</p>
+                    </div>
+                )}
+            </div>
+
+            {selectedGuiaRemision && (
+                <GuiaRemisionModal
+                    f={selectedGuiaRemision}
+                    onClose={() => setSelectedGuiaRemision(null)}
+                />
+            )}
+        </div>
+    )
+}
