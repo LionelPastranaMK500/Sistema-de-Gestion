a0a1220fa7c9f716b417d82596a8b249f93ee800
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2025-09-20 18:21:07 -0500

    feat(common): se agregro el componente NotFound para rutas 404

---
+++ src/modules/common/NotFound.jsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+    return (
+        <div>
+            <div>
+                <h1>404</h1>
+                <p>Pagina no encontrada</p>
+                <a href="/dashboard">Volver al inicio</a>
+            </div>
+        </div>
+    );
+}
