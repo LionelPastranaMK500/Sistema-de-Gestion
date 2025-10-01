c1e383aed14b66307d249539124346bf063af774
LionelPastranaMK500 <andriypastrana1@gmail.com>
2025-09-30 22:45:57 -0500

    feat(PublicRoute): mejorar la lógica de carga y transición del loader

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -1,22 +1,44 @@
+// src/routes/PublicRoute.jsx
 import { Navigate, Outlet } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { CSSTransition } from "react-transition-group";
 import { getActiveUser } from "@services/auth/authServices";
 import PublicLoader from "@components/Loaders/PublicLoader";
-import { useEffect, useState } from "react";
+import useDelay from "@utils/redirectWithDelay.js"; // o @utils/useDelay.js
 
 const PublicRoute = () => {
-    const [loading, setLoading] = useState(true);
     const [user, setUser] = useState(null);
+
     useEffect(() => {
         const activeUser = getActiveUser();
         setUser(activeUser);
-        setLoading(false);
     }, []);
-    if (loading) {
-        return <PublicLoader />;
-    }
-    if (user) {
-        return <Navigate to="/welcome" replace />;
-    }
-    return <Outlet />;
-}
+
+    const TOTAL_MS = 7000;     // tiempo total deseado
+    const FADE_MS = 2000;     // mismo para aparecer/desaparecer (ajústalo si quieres)
+    const HOLD_MS = Math.max(0, TOTAL_MS - 2 * FADE_MS);
+
+    const gateDone = useDelay(HOLD_MS);
+    const showLoader = !gateDone;
+
+    const nodeRef = useRef(null);
+
+    return (
+        <>
+            <CSSTransition
+                in={showLoader}
+                timeout={FADE_MS}      // coincide con CSS
+                classNames="fade"
+                unmountOnExit
+                appear
+                nodeRef={nodeRef}
+            >
+                <PublicLoader ref={nodeRef} />
+            </CSSTransition>
+
+            {!showLoader && (user ? <Navigate to="/welcome" replace /> : <Outlet />)}
+        </>
+    );
+};
+
 export default PublicRoute;
