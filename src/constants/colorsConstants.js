aac3612443f0415e5f30ee24882ed115b51eb5c3
LionelPastranaMK500 <andriypastrana1@gmail.com>
2025-09-30 22:45:47 -0500

    fix(colors): corregir colores de botones para mejorar la consistencia

--- src/constants/colorsConstants.js
+++ src/constants/colorsConstants.js
@@ -1,11 +1,11 @@
 export const buttonColors = [
     "bg-green-500",   // Estadísticas
-    "bg-teal-600",    // Realizar venta
+    "bg-amber-700",    // Realizar venta
     "bg-yellow-500",  // Ventas realizadas
     "bg-purple-500",  // Proformas
     "bg-red-500",     // Reportes            
-    "bg-pink-500",    // Clientes y proveedores
-    "bg-cyan-500",    // Productos y servicios
-    "bg-gray-500",    // Configuración
-    "bg-orange-500"   //Guia Remision
+    "bg-lime-400",     //Guia Remision
+    "bg-cyan-500",     // Clientes y proveedores
+    "bg-gray-500",   //Productos y servicios
+    "bg-orange-500"    // Configuración
 ];
