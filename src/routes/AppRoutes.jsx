b254012f0bd94fb9eda79c5f0bb30ce5ec35e883
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2025-11-23 22:38:23 -0500

    refactor(routes): actualizar imports de rutas
    Actualizar configuración de enrutamiento para usar nuevas rutas de importación.

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,41 +4,38 @@ import MainLayout from "@layouts/MainLayout";
 import AuthLayout from "@layouts/AuthLayout";
 import PublicRoute from "./PublicRoute";
 import PrivateRoute from "./PrivateRoute";
-import PublicLoader from "@components/Loaders/PublicLoader";
-import ContentLoader from "@components/Loaders/ContentLoader";
+import PublicLoader from "@components/feedback/PublicLoader";
+import ContentLoader from "@components/feedback/ContentLoader";
 
 //modulos common
 import NotFound from "@modules/common/NotFound";
 //modulos publi
-import Login from "@modules/auth/Login";
-import Register from "@modules/auth/Register";
-import ResetPassword from "@modules/auth/ResetPassword";
-
+import Login from "@auth/Login";
+import Register from "@auth/Register";
+import ResetPassword from "@auth/ResetPassword";
 //modulos priv
-const SunatForm = lazy(() => import("@modules/auth/SunatForm"));
-const Dashboard = lazy(() => import("@modules/dashboard/Dashboard"));
-const EstadisticasView = lazy(() => import("@modules/estadisticas/EstadisticasView"));
-const VentasView = lazy(() => import("@modules/ventas/VentasView"));
-const GuiaRemision = lazy(() => import("@modules/guia_remision/GuiaRemisionView"));
-const FacturasView = lazy(() => import("@modules/facturas/FacturasView"));
-const ProformasView = lazy(() => import("@modules/proformas/ProformasView"));
-const ClienteView = lazy(() => import("@modules/clientes/ClientesView"));
-const ProductosView = lazy(() => import("@modules/productos/ProductosView"));
-
+const SunatForm = lazy(() => import("@auth/SunatForm"));
+const Dashboard = lazy(() => import("@dashboard/Dashboard"));
+const EstadisticasView = lazy(() => import("@estadistica/EstadisticasView"));
+const VentasView = lazy(() => import("@ventas/VentasView"));
+const GuiaRemision = lazy(() => import("@guias/GuiaRemisionView"));
+const FacturasView = lazy(() => import("@facturas/FacturasView"));
+const ProformasView = lazy(() => import("@proformas/ProformasView"));
+const ClienteView = lazy(() => import("@cliente/ClientesView"));
+const ProductosView = lazy(() => import("@productos/ProductosView"));
 //modulo config
-const ConfiguracionView = lazy(() => import("@modules/configuracion/ConfiguracionView"));
-const ConfigurarEmpresa = lazy(() => import("@modules/configuracion/items/ConfigurarEmpresa"));
-const ConfigurarUsuario = lazy(() => import("@modules/configuracion/items/ConfigurarUsuario"));
-const ConfigurarAlmacen = lazy(() => import("@modules/configuracion/items/ConfigurarAlmacen"));
-const ConfigurarSucursal = lazy(() => import("@modules/configuracion/items/ConfigurarSucursal"));
-const ConfigurarImpresion = lazy(() => import("@modules/configuracion/items/ConfigurarImpresion"));
-
+const ConfiguracionView = lazy(() => import("@configuracion/ConfiguracionView"));
+const ConfigurarEmpresa = lazy(() => import("@configuracion/pages/ConfigurarEmpresa"));
+const ConfigurarUsuario = lazy(() => import("@configuracion/pages/ConfigurarUsuario"));
+const ConfigurarAlmacen = lazy(() => import("@configuracion/pages/ConfigurarAlmacen"));
+const ConfigurarSucursal = lazy(() => import("@configuracion/pages/ConfigurarSucursal"));
+const ConfigurarImpresion = lazy(() => import("@configuracion/pages/ConfigurarImpresion"));
 //modulo reportes
-const ReportesView = lazy(() => import("@modules/reportes/ReportesView"));
-const ReporteVentasGeneral = lazy(() => import("@modules/reportes/items/ReporteVentasGeneral"));
-const ReporteClientesProveedores = lazy(() => import("@modules/reportes/items/ReporteClientesProveedores"));
-const ReporteProductos = lazy(() => import("@modules/reportes/items/ReporteProductos"));
-const ReporteGuiasRemision = lazy(() => import("@modules/reportes/items/ReporteGuiasRemision"));
+const ReportesView = lazy(() => import("@reportes/ReportesView"));
+const ReporteVentasGeneral = lazy(() => import("@reportes/components/ReporteVentasGeneral"));
+const ReporteClientesProveedores = lazy(() => import("@reportes/components/ReporteClientesProveedores"));
+const ReporteProductos = lazy(() => import("@reportes/components/ReporteProductos"));
+const ReporteGuiasRemision = lazy(() => import("@reportes/components/ReporteGuiasRemision"));
 
 
 const AppRoutes = () => {
--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -1,10 +1,9 @@
-// src/routes/PublicRoute.jsx
 import { Navigate, Outlet } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
 import { CSSTransition } from "react-transition-group";
 import { getActiveUser } from "@services/auth/authServices";
-import PublicLoader from "@components/Loaders/PublicLoader";
-import useDelay from "@utils/redirectWithDelay.js";
+import PublicLoader from "@/components/feedback/PublicLoader";
+import useDelay from "@utils/navigation/redirectWithDelay.js";
 
 const PublicRoute = () => {
     const [user, setUser] = useState(null);
@@ -18,7 +17,6 @@ const PublicRoute = () => {
     const HOLD_MS = Math.max(0, TOTAL_MS - 2 * FADE_MS); // = 3000
     const VISIBLE_MS = FADE_MS + HOLD_MS;                  // = 5000  (2s in + 3s hold)
 
-    // ⬅️ el loader estará montado VISIBLE_MS y luego hará fade-out por FADE_MS
     const gateDone = useDelay(VISIBLE_MS);
     const showLoader = !gateDone;
 
